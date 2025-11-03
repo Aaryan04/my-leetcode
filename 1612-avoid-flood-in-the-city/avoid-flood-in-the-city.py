05fff92e18c11bd6697715d30c917f561ac5245e
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-11-02 18:41:50 -0800

    Time: 384 ms (6.02%) | Memory: 34.1 MB (70.75%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
@@ -1,21 +1,24 @@
-from sortedcontainers import SortedList
-
-
 class Solution:
     def avoidFlood(self, rains: List[int]) -> List[int]:
-        ans = [1] * len(rains)
-        st = SortedList()
-        mp = {}
-        for i, rain in enumerate(rains):
-            if rain == 0:
-                st.add(i)
-            else:
-                ans[i] = -1
-                if rain in mp:
-                    it = st.bisect(mp[rain])
-                    if it == len(st):
+        last_rain_on = defaultdict(int)
+        zero_idx = []
+        ans = [1]*len(rains)
+
+        for i, lake in enumerate(rains):
+            if lake > 0:                   # raining on the lake
+                if lake in last_rain_on:       # if already full, we need to dry it before today
+                    # find earliest dry day after full[lake]
+                    idx = bisect.bisect_right(zero_idx, last_rain_on[lake])
+                    if idx == len(zero_idx):        # no dry day possible
                         return []
-                    ans[st[it]] = rain
-                    st.discard(st[it])
-                mp[rain] = i
+                    dry_day = zero_idx.pop(idx)     # use that day
+                    ans[dry_day] = lake             # and update that dry day in the ans
+
+                # make this lake as now filled (rained today)
+                last_rain_on[lake] = i
+                ans[i] = -1
+                
+            else:                           # found 0
+                zero_idx.append(i)
+                ans[i] = 1                  # placeholder for now (may change later)
         return ans

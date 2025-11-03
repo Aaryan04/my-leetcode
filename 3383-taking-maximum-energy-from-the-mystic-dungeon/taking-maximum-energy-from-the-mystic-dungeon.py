1db78598522dd7f562258707fde6612a5330c58e
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-11-02 22:28:19 -0800

    Time: 1237 ms (12.74%) | Memory: 31.2 MB (37.79%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.py
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.py
@@ -5,33 +5,35 @@ class Solution:
         # f(i) = energy[i] otherwise
         # ans will max( f(i) ) for all i in [0, n-1]
         
-        def dfs(i):
-            if i >= n:          # base case
-                return 0
-            if i in memo:
-                return memo[i]
-            # take energy[i] and jump
-            next_energy = dfs(i+k) if i + k < n else 0
-            memo[i] = energy[i] + next_energy
-            return memo[i]
+        # def dfs(i):
+        #     if i >= n:          # base case
+        #         return 0
+        #     if i in memo:
+        #         return memo[i]
+        #     # take energy[i] and jump
+        #     next_energy = dfs(i+k) if i + k < n else 0
+        #     memo[i] = energy[i] + next_energy
+        #     return memo[i]
         
+        # n = len(energy)
+        # memo = {}
+        # ans = -math.inf
+        # for i in range(n):
+        #     ans = max(ans, dfs(i))
+        # return ans
+
+
         n = len(energy)
-        memo = {}
         ans = -math.inf
-        for i in range(n):
-            ans = max(ans, dfs(i))
+
+        for i in range(n-k, n):
+            total = 0
+            j = i
+            while j >= 0:
+                total += energy[j]
+                j = j-k
+                ans = max(ans, total)
+
         return ans
 
-
-        # n = len(energy)
-        # ans = -math.inf
-
-        # for i in range(n-k, n):
-        #     total = 0
-        #     j = i
-        #     while j >= 0:
-        #         total += energy[j]
-        #         j = j-k
-        #         ans = max(ans, total)
-
-        # return ans
+        # THIS RUNS FASTER!

100bd7ec4a52675fc6a05a4d58b47c7a0b6750d8
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-11-11 19:43:56 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (26.14%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,39 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # f(i, j) = num of ways to reach cell (i, j)
-        dp = [[0]*n for _ in range(m)]
-        dp[0][0] = 1        # base case
+        # f(i,j) -> no of ways to reach (0, 0) => (i, j)
 
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    continue            # base case
-                
-                # copy the exact same recurrence logic in this
-                up = dp[i-1][j] if i > 0 else 0
-                left = dp[i][j-1] if j > 0 else 0
-                
-                dp[i][j] = up + left
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
 
-        return dp[m-1][n-1]
+        def dfs(i, j):
+            # BASE CASES
+            # reached destination
+            if i == 0 and j == 0:
+                return 1
 
+            # out of bound condition
+            if i < 0 or j < 0:
+                return 0
 
-
-        # MEMOIZATION SOLUTION
-        # dp = [[-1 for _ in range(n)] for _ in range(m)]
-
-        # def dfs(i, j):
-        #     if i == 0 and j == 0:
-        #         return 1
-        #     if i < 0 or j < 0:
-        #         return 0
-        #     if dp[i][j] != -1:
-        #         return dp[i][j]
+            if dp[i][j] != -1:
+                return dp[i][j]
             
-        #     up = dfs(i-1, j)
-        #     left = dfs(i, j-1)
-        #     dp[i][j] = up + left
-            
-        #     return dp[i][j]
-        
-        # return dfs(m-1, n-1)
+            # do all stuff on i, j
+            up = dfs(i-1,  j)
+            left = dfs(i, j-1)
+            dp[i][j] = up + left
+
+            return dp[i][j]        # cnt ways by summing
+
+        return dfs(m-1,n-1)

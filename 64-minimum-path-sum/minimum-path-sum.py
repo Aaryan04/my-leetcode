6eb10245cf54a5169491a2095a3cd7f9693364ff
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-11-11 22:10:49 -0800

    Time: 32 ms (22.85%) | Memory: 21.5 MB (15.54%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        # f(i, j) -> min sum of path to reach i, j from (0, 0)
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        def dfs(i, j):
+            if i == 0 and j == 0:           # if we find 0, 0 then return the grid[0][0] value
+                return grid[0][0]
+            
+            if i < 0 or j < 0:
+                return math.inf             # return a number with is ignored while calc min path sum
+
+            if dp[i][j] != -1:
+                return dp[i][j]
+
+            up = grid[i][j] + dfs(i-1, j)
+            left = grid[i][j] + dfs(i, j-1)
+            dp[i][j] = min(up, left)
+            return dp[i][j]
+
+        return dfs(m-1, n-1)

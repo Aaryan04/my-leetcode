a1e22eb06ff0cdaf35880d7dd440926e45671462
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-11-21 18:55:42 -0800

    Time: 0 ms (100.00%) | Memory: 20.1 MB (67.66%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.py
+++ 419-battleships-in-a-board/battleships-in-a-board.py
@@ -1,9 +1,12 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
+        # RESOLVE THIS PROBLEM AGAIN
+        
         ROWS = len(board)
         COLS = len(board[0])
         ships = 0
 
+        # we will just cnt the top left of each ship grp, if left or up cell have a ship then its not a ship
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] != 'X':

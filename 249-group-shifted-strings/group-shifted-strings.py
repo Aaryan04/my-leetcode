8c9e4a8b296beab2e1aedc74088ad58a3d58406b
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-12-02 14:53:16 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (21.10%) - LeetSync

---
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        hashmap = {}
+
+        for s in strings:
+            key_list = []
+            for i in range(len(s) - 1):
+                diff = (ord(s[i+1]) - ord(s[i]) + 26) % 26   # circular difference = next - curr + 26
+                key_list.append(diff)
+
+            key = tuple(key_list)
+
+            if key not in hashmap:
+                hashmap[key] = []
+            hashmap[key].append(s)
+
+        return list(hashmap.values())
+            
+

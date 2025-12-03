31eea31e55f8ae7b75ea1ac85ba7e547eb51c066
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-12-02 20:55:21 -0800

    Time: 8 ms (76.52%) | Memory: 18 MB (74.07%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,14 +1,33 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
+        # O(n^2) solution
+        # n = len(boxes)
+        # one_idx = set([i for i in range(n) if boxes[i] == '1'])
+
+        # ans = []
+        # for i in range(n):
+        #     cnt = 0
+        #     for idx in one_idx:
+        #         cnt += abs(idx - i)
+        #     ans.append(cnt)
+        # return ans
+
         n = len(boxes)
-        one_idx = set([i for i in range(n) if boxes[i] == '1'])
-        print(one_idx)
-
-        ans = []
+        ans = [0] * n
+        cnt = 0
+        ops = 0
         for i in range(n):
-            cnt = 0
-            for idx in one_idx:
-                cnt += abs(idx - i)
-            ans.append(cnt)
-        return ans
+            ans[i] += ops
+            if boxes[i] == '1':
+                cnt += 1
+            ops += cnt
 
+        cnt = 0
+        ops = 0
+        for i in range(n - 1, -1, -1):
+            ans[i] += ops
+            if boxes[i] == '1':
+                cnt += 1
+            ops += cnt
+
+        return ans

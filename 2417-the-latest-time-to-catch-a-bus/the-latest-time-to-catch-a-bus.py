48a2856e49936148992eecc42e48b984660d2342
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-12-04 12:40:55 -0800

    Time: 87 ms (83.79%) | Memory: 36.8 MB (74.96%) - LeetSync

---
+++ 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.py
@@ -0,0 +1,35 @@
+class Solution:
+    def latestTimeCatchTheBus(self, buses, passengers, totalCapacity):
+        buses.sort()
+        passengers.sort()
+        
+        i, j = 0, 0
+        n, m = len(buses), len(passengers)
+        currentCapacity = 0
+        
+        while i < n:
+            currentCapacity = 0
+            while j < m and currentCapacity < totalCapacity and passengers[j] <= buses[i]:
+                j += 1
+                currentCapacity += 1
+            
+            # Last Bus
+            if i == n - 1:
+                j -= 1
+                if currentCapacity < totalCapacity:
+                    time = buses[i]
+                    while j >= 0 and time == passengers[j]:
+                        time -= 1
+                        j -= 1
+                    return time
+                else:
+                    time = passengers[j] - 1
+                    j -= 1
+                    while j >= 0 and time == passengers[j]:
+                        time -= 1
+                        j -= 1
+                    return time
+            
+            i += 1
+        
+        return buses[-1]

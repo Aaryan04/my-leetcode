858b300e423f96c3a3c61acbaad8546b7c0f596d
Aaryan Shah <72094642+Aaryan04@users.noreply.github.com>
2025-12-07 21:42:13 -0800

    Time: 462 ms (44.32%) | Memory: 65.4 MB (25.76%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,23 +1,41 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        # MEMOIZATION SOLUTION
+        # t = amount
+        # n = len(coins)
+        # dp = [[-1] * (t+1) for _ in range(n)]
+
+        # def f(i, t):
+        #     if i == 0:
+        #         return 1 if (t % coins[0] == 0) else 0 
+        #     if dp[i][t] != -1:
+        #         return dp[i][t]
+
+        #     not_take = f(i-1, t)
+        #     take = 0
+        #     if coins[i] <= t:
+        #         take = f(i, t-coins[i])        # we dont move back the i because we can reuse the same coin again
+            
+        #     dp[i][t] = not_take + take
+            
+        #     return dp[i][t]
+        
+        # return f(n-1, t)
+                
+        # TABULATION SOLUTION
         t = amount
         n = len(coins)
         dp = [[-1] * (t+1) for _ in range(n)]
 
-        def f(i, t):
-            if i == 0:
-                return 1 if (t % coins[0] == 0) else 0 
-            if dp[i][t] != -1:
-                return dp[i][t]
-
-            not_take = f(i-1, t)
-            take = 0
-            if coins[i] <= t:
-                take = f(i, t-coins[i])        # we dont move back the i because we can reuse the same coin again
-            
-            dp[i][t] = not_take + take
-            
-            return dp[i][t]
+        for t in range(t+1):
+            dp[0][t] = 1 if (t % coins[0] == 0) else 0 
         
-        return f(n-1, t)
-                
+        for i in range(1, n):
+            for t in range(t+1):
+                not_take = dp[i-1][t]
+                take = 0
+                if coins[i] <= t:
+                    take = dp[i][t-coins[i]]
+                dp[i][t] = take + not_take
+            
+        return dp[n-1][t]
